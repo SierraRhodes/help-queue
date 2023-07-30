b84c3318fdea7ea0669e93eca33c9c46eb71b385
SierraRhodes <alreadywrote@gmail.com>
2023-07-30 15:56:49 -0700

    add working tickets

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,11 @@ import Header from "./Header";
 import TicketList from "./TicketList";
 
 function App() {
-  const name = "Thato";
-  const name2 = "Haley";
   return (
     <React.Fragment>
       <Header/>
       <TicketList/>
       <p><em>Firebase entries not saving!</em></p>
-      {/* This is a JSX comment. */}
       <hr/>
     </React.Fragment>
   );
