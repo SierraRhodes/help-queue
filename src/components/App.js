2f5d91c1536500ae7ce32d8d8259e57f187cfac9
SierraRhodes <alreadywrote@gmail.com>
2023-07-31 19:27:03 -0700

    add click handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import TicketList from "./TicketList";
+import TicketControl from "./TicketControl";
 
 function App() {
   return (
     <React.Fragment>
       <Header/>
       <TicketList/>
-      <p><em>Firebase entries not saving!</em></p>
-      <hr/>
+      <TicketControl/>
     </React.Fragment>
   );
 }
