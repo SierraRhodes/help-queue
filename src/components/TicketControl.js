d3aac12553b754269a269d56c5ed051dc00e82b8
SierraRhodes <alreadywrote@gmail.com>
2023-08-06 18:21:25 -0700

    add ticket details

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,20 +1,29 @@
 import React from  'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
 
 class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       formVisibleOnPage: false,
-      mainTicketList: []
+      mainTicketList: [],
+      selectedTicket: null
     };
 }
   
 handleClick = () => {
-  this.setState(prevState => ({
-    formVisibleOnPage: !prevState.formVisibleOnPage
-  })); 
+  if (this.state.selectedTicket != null) {
+    this.setState({
+      formVisibleOnPage: false,
+      selectedTicket: null
+    });
+  } else {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage,
+    }));
+  }
 }
 
 handleAddingNewTicketToList = (newTicket) => {
@@ -22,14 +31,24 @@ handleAddingNewTicketToList = (newTicket) => {
   this.setState({mainTicketList: newMainTicketList, formVisibleOnPage: false});
 }
 
+handleChangingSelectedTicket = (id) => {
+  const selectedTicket = this.state.mainTicketList.filter(ticket => ticket.id === id)[0];
+    this.setState({selectedTicket: selectedTicket});
+}
+
 render(){
   let currentlyVisibleState = null;
   let buttonText = null;
-  if (this.state.formVisibleOnPage) {
+  
+  if (this.state.selectedTicket != null) {
+    currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket}/>
+    buttonText = "Return to Ticket List";
+  }
+  else if (this.state.formVisibleOnPage) {
     currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />;
     buttonText = "Return to Ticket List";
   } else {
-    currentlyVisibleState  = <TicketList ticketList={this.state.mainTicketList}/>;
+    currentlyVisibleState  = <TicketList ticketList={this.state.mainTicketList} onTicketSelection={this.handleChangingSelectedTicket}/>;
     buttonText = "Add Ticket";
   }
   return ( 
