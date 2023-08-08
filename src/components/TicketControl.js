b9c3951118844fef534eea43cb248b42ceb6c543
SierraRhodes <alreadywrote@gmail.com>
2023-08-07 20:24:41 -0700

    add delete functionality

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -36,12 +36,19 @@ handleChangingSelectedTicket = (id) => {
     this.setState({selectedTicket: selectedTicket});
 }
 
+handleDeletingTicket = (id) => {
+  const newMainTicketList = this.state.mainTicketList.filter(ticket => ticket.id !== id);
+  this.setState({
+    mainTicketList: newMainTicketList,
+    selectedTicket: null
+  });
+}
 render(){
   let currentlyVisibleState = null;
   let buttonText = null;
   
   if (this.state.selectedTicket != null) {
-    currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket}/>
+    currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} onClickingDelete = {this.handleDeletingTicket} />
     buttonText = "Return to Ticket List";
   }
   else if (this.state.formVisibleOnPage) {
